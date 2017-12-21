16ca67990dd4b5ec3406364fcb6566edebc3180d
romanvoinarovych <romanvoynar@gmail.com>
2017-12-21 22:14:30 +0200

    exam_3

---
+++ js/main.js
@@ -0,0 +1,20 @@
+;(function($){
+	"use strict";
+	$(function(){
+		$('.cd-slider').slick({
+			// dots: true,
+			// infinite: true,
+			// speed: 300,
+			// slidesToShow: 1,
+			// adaptiveHeight: true,
+			// cssEase: 'ease-in',
+			// adaptiveHeight: true,
+			// touchMove: true,
+			// slide: ".cd-slider-item",
+			// focusOnSelect: false,
+			// prevArrow: ".cd-slider-prev",
+			// nextArrow: ".cd-slider-next"
+		});
+	});
+})(jQuery);
+
